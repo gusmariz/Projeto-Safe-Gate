ec5d25afe5064bc2ec1514e4ea5e2d022a562f94
Gustavo Mariz <gustavo241@gmail.com>
2025-05-11 13:49:37 -0300

    Pasta back-end na branch tela-ph

---
+++ back-end/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json);
+
+app.post("/home", (req, res) => {
+    console.log("Usuário abriu");
+    res.json(200).json({ status: "recebido" });
+})
