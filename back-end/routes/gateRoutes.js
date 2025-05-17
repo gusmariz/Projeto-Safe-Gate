bfb12d352961c3f77ddb465e20cbff6311a5a5b8
Gustavo Mariz <gustavo241@gmail.com>
2025-05-17 17:00:23 -0300

    Correção do gateRoutes

--- back-end/routes/gateRoutes.js
+++ back-end/routes/gateRoutes.js
@@ -1,4 +1,11 @@
-import {verifyToken} from '../middlewares/authMiddleware.js';
+import express from 'express';
+import { controlGate, history } from '../controllers/gateController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
+const router = express.Router();
+
+// Rotas protegidas por JWT
 router.post('/action', verifyToken, controlGate);
 router.get('/history', verifyToken, history);
+
+export default router;
