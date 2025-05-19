b82264d518737cda0eaa25a015317cf1c94786a8
Gustavo Mariz <gustavo241@gmail.com>
2025-05-19 11:44:46 -0300

    Função para verificar conexão com Arduino e enviar ação para ele

--- back-end/controllers/gateController.js
+++ back-end/controllers/gateController.js
@@ -1,5 +1,12 @@
 import jwt from 'jsonwebtoken';
+import { SerialPort } from 'serialport';
 import { createAction, getHistory } from '../models/GateAction.js';
+
+const port = new SerialPort({
+    path: 'COM3',
+    baudRate: 9600,
+    autoOpen: false,
+});
 
 export const controlGate = async (req, res) => {
     try {
@@ -13,8 +20,17 @@ export const controlGate = async (req, res) => {
             return res.status(400).json({ error: 'Ação inválida' });
         }
 
-        await createAction(decoded.id, acao);
-        res.json({ message: `Portão ${acao} com sucesso!` });
+        if (!port.isOpen) await port.open(); // Abre a conexão se necessário
+        port.write(`${acao}\n`, async (error) => {
+            if (error) {
+                console.error('Erro ao enviar para Arduino:', error);
+                return res.status(500).json({ error: 'Falha na comunicação serial' });
+            }
+
+            await createAction(decoded.id, acao);
+            res.json({ message: `Comando "${acao}" enviado ao Arduino!` });
+        });
+    
     } catch (error) {
         console.log('Erro no controle do portão:', error);
         res.status(500).json({ error: 'Erro no servidor' });
