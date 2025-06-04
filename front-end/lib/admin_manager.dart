7670083c7098552081c16f7ed07076909a967365
Gustavo Mariz <gustavo241@gmail.com>
2025-06-04 17:05:10 -0300

    Dois arquivos criados para a tela de vizualizar usuários e excluí-los

---
+++ front-end/lib/admin_manager.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AdminManager {
+  final String token;
+
+  AdminManager(this.token);
+
+  Future<List<Map<String , dynamic>>> fetchUsuarios() async {
+    final response = await http.get(
+      Uri.parse('https://projeto-safe-gate-production.up.railway.app/admin/usuarios'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      return List<Map<String, dynamic>>.from(jsonDecode(response.body));
+    } else {
+      throw Exception('Erro ao carregar usuários');
+    }
+  }
+
+  Future<void> deleteUsuario(String email) async {
+    final response = await http.delete(
+      Uri.parse('https://projeto-safe-gate-production.up.railway.app/admin/usuarios/$email'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Erro ao excluir usuário');
+    }
+  }
+}
